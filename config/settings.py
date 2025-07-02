07d3ac7abfa4df12c9ab6cf615a757fe460e5a6a
muqimbayev <muqimbayev@gmail.com>
2025-07-02 20:29:25 +0500

    tayyor 1.0

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ap9wt@6d-u@xqs$^0e=ax60y85unzaj@fdvrnxkx$yel8732@8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
